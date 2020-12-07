c6347f3405095f0fadc9c497bcd6c97b83d85a4d
desk2000 <52657198+desk2000@users.noreply.github.com>
2020-12-07 17:07:12 +0800

    sign in page

--- farm-v1/src/components/FormLogIn.js
+++ farm-v1/src/components/FormLogIn.js
@@ -15,44 +15,46 @@ function FormLogIn({ submitForm, userData, setUserData }) {
 			{userData.loggedIn ? (
 				<Redirect to='/home' />
 			) : (
-				<form>
-					<h1>Welcome back!</h1>
-					<div className='form-inputs'>
-						<label className='form-label'>Username</label>
-						<input
-							className='form-input'
-							type='text'
-							name='username'
-							placeholder='Enter your username'
-							value={values.username}
-							onChange={handleChange}
-						/>
-						{errors.username && <p>{errors.username}</p>}
-					</div>
-					<div className='form-inputs'>
-						<label className='form-label'>Password</label>
-						<input
-							className='form-input'
-							type='password'
-							name='password'
-							placeholder='Enter your password'
-							value={values.password}
-							onChange={handleChange}
-						/>
-						{errors.password && <p>{errors.password}</p>}
-					</div>
-					<button
-						className='form-input-btn'
-						type='submit'
-						onClick={submitForm}
-					>
-						Log In
-					</button>
-					<span className='form-input-login'>
-						Want a new account? Signup
-						<Link to='/entrance/signup'> here</Link>.
-					</span>
-				</form>
+				<div className='form-content-right'>
+					<form className='form' noValidate>
+						<h1>Welcome back!</h1>
+						<div className='form-inputs'>
+							<label className='form-label'>Username</label>
+							<input
+								className='form-input'
+								type='text'
+								name='username'
+								placeholder='Enter your username'
+								value={values.username}
+								onChange={handleChange}
+							/>
+							{errors.username && <p>{errors.username}</p>}
+						</div>
+						<div className='form-inputs'>
+							<label className='form-label'>Password</label>
+							<input
+								className='form-input'
+								type='password'
+								name='password'
+								placeholder='Enter your password'
+								value={values.password}
+								onChange={handleChange}
+							/>
+							{errors.password && <p>{errors.password}</p>}
+						</div>
+						<button
+							className='form-input-btn'
+							type='submit'
+							onClick={submitForm}
+						>
+							Log In
+						</button>
+						<span className='form-input-login'>
+							Want a new account? Signup
+							<Link to='/entrance/signup'> here</Link>.
+						</span>
+					</form>
+				</div>
 			)}
 		</>
 	);
