1963447d943617b061bc73ed3ee732b0821fcb29
gary1030 <gary6658@ntu.im>
2020-12-07 23:04:30 +0800

    minor change

--- farm-v1/src/pages/Friends.js
+++ farm-v1/src/pages/Friends.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 
 function Friends() {
+	const [friendList, setFriendList] = useState([]);
+
+	
 	return (
 		<>
 			<div className='friends'>
 				<h1>Friends</h1>
 			</div>
+			{
+				friendList.map((friend, key) => {
+            		return <div key={key}>{friend.name}</div>
+        		})
+			}
 		</>
 	);
 }
