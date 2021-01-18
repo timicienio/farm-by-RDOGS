b5f9e913af79e5dd7d65d6e167a3cb29e1baedc9
gary1030 <gary6658@ntu.im>
2021-01-18 13:53:04 +0800

    minor change

--- src/hooks/useFarms.js
+++ src/hooks/useFarms.js
@@ -4,8 +4,10 @@ import { useQuery, useMutation } from '@apollo/react-hooks';
 import { CREATE_FARM_MUTATION, GET_FARMS_QUERY } from '../graphql';
 
 const useFarms = () => {
-	const [hasSetFarmList, setHasSetFarmList] = useState(false);
-	const [farmList, setFarmList] = useState([]);
+    
+    //const [hasSetFarmList, setHasSetFarmList] = useState(false);
+    const { data:{ getFarms } } = useQuery(GET_FARMS_QUERY);
+	//const [farmList, setFarmList] = useState([]);
 	// record the farm name which will be created
 	const [createFarmName, setCreateFarmName] = useState(false);
 
@@ -27,15 +29,15 @@ const useFarms = () => {
 		setCreateFarmName(e.target.value);
 	};
 
-	useEffect(() => {
-		if (!hasSetFarmList) {
-			const { data } = useQuery(GET_FARMS_QUERY);
-			setFarmList(data.getFarms);
-			setHasSetFarmList(true);
-		}
-	}, [hasSetFarmList]);
+	// useEffect(() => {
+	// 	if (!hasSetFarmList) {
+	// 		const { data } = useQuery(GET_FARMS_QUERY);
+	// 		setFarmList(data.getFarms);
+	// 		setHasSetFarmList(true);
+	// 	}
+	// }, [hasSetFarmList]);
 
-	return { handleChange, createNewFarm, farmList, createFarmName };
+	return { handleChange, createNewFarm, getFarms, createFarmName };
 };
 
 export default { useFarms };
