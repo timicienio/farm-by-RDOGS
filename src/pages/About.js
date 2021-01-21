c9939556c4bf81770a1779e32604813830428450
fredred375 <58181823+fredred375@users.noreply.github.com>
2021-01-21 15:31:05 +0800

    almost

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import RDOGE from './pic/RDOGE.png'
+import './About.css'
 
 function About() {
 	return (
 		<>
 			<div className='about'>
 				<h1>About</h1>
+				<br/>
+				<img src={RDOGE} alt="funny doge" className="doge"/>
+				<br/>
+				<h3>Made by RDOGS</h3>
+				<br/>
+				<h3>noice</h3>
 			</div>
 		</>
 	);
