b6f2eaf47cd4657d34d684cecdb4dd3cc064d594
gary1030 <gary6658@ntu.im>
2021-01-21 11:27:27 +0800

    add ApolloProvider

--- src/index.js
+++ src/index.js
@@ -1,74 +1,8 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-
-import { ApolloClient, InMemoryCache } from 'apollo-boost';
-import { ApolloProvider } from 'react-apollo';
-import { split } from 'apollo-link';
-import { HttpLink } from 'apollo-link-http';
-import { WebSocketLink } from 'apollo-link-ws';
-import { getMainDefinition } from 'apollo-utilities';
-//import { onError } from 'apollo-link-error'
-import { setContext } from 'apollo-link-context';
-import { AuthProvider } from './context/auth';
-
-
 import * as serviceWorker from './serviceWorker';
+import ApolloProvider from './ApolloProvider';
 
-// Create an http link:
-const httpLink = new HttpLink({
-	uri: 'https://rdogs-farm.herokuapp.com/',
-});
-
-// Create a WebSocket link:
-const wsLink = new WebSocketLink({
-	uri: `wss://rdogs-farm.herokuapp.com/graphql`,
-	options: { reconnect: true },
-});
-
-// using the ability to split links, you can send data to each link
-// depending on what kind of operation is being sent
-const authLink = setContext(() => {
-	const token = localStorage.getItem('jwtToken');
-	return {
-	  headers: {
-		  Authorization: token ? `Bearer ${token}` : ''
-		}
-	};
-});
-const link = split(
-	// split based on operation type
-	({ query }) => {
-		const definition = getMainDefinition(query);
-		return (
-			definition.kind === 'OperationDefinition' &&
-			definition.operation === 'subscription'
-		);
-	},
-	wsLink,
-	authLink.concat(httpLink)
-);
-
-
-
-const client = new ApolloClient({
-	link,
-	cache: new InMemoryCache().restore({}),
-	onError: ({ networkError, graphQLErrors }) => {
-		console.log('graphQLErrors', graphQLErrors)
-		console.log('networkError', networkError)
-	}
-});
-
-const wrappedApp = (
-	<ApolloProvider client={client}>
-		<AuthProvider>
-			<App />
-		</AuthProvider>
-	</ApolloProvider>
-);
-
-ReactDOM.render(wrappedApp, document.getElementById('root'));
+ReactDOM.render(ApolloProvider, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
